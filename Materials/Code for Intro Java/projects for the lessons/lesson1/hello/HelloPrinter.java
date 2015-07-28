e70926d382c01813134099d786bcf213b152d0b0
Kevin Amiranoff <kevin.amiranoff@gmail.com>
2015-07-29 00:01:40 +0200

    adds a few print and comments to HelloPrinter.java

--- Materials/Code for Intro Java/projects for the lessons/lesson1/hello/HelloPrinter.java
+++ Materials/Code for Intro Java/projects for the lessons/lesson1/hello/HelloPrinter.java
@@ -1,10 +1,28 @@
+//watching lesson 2. 01 introduction
+
 public class HelloPrinter
 {
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
         System.out.println("Hello");
+        System.out.print(1+3);
         System.out.println(1+3);
         System.out.println("1+3");
+        //System.out.println(1/0);
+
     }
+
+
+    //Pseudocode
+    //computation : total cost = purchase price + cost of gas
+    //decision : if total cost 1 < total cost 2
+    //repetition : while blance < 10000
+    //              Repeat 10 times
+    //
+    //
+    //Algorithm :
+    // unambiguous
+    // executable
+    // terminating
 }
