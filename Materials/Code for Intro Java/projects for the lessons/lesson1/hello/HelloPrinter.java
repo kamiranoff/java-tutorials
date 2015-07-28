3e92221d04fd1d79cd124f854197cb5f42cb711f
Kevin Amiranoff <kevin.amiranoff@gmail.com>
2015-07-28 23:23:24 +0200

    init commit

---
+++ Materials/Code for Intro Java/projects for the lessons/lesson1/hello/HelloPrinter.java
@@ -0,0 +1,10 @@
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("Hello");
+        System.out.println(1+3);
+        System.out.println("1+3");
+    }
+}
